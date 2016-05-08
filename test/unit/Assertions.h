ab26e248dff60e3276ffdfd188e12d07d008e11e
tylerreisinger <reisinger.tyler@gmail.com>
2016-05-08 19:19:09 -0400

    Added color testing assertions for far better unit test messages.

--- test/unit/Assertions.h
+++ test/unit/Assertions.h
@@ -2,6 +2,9 @@
 
 #include <type_traits>
 #include <cmath>
+#include <sstream>
+
+#include "Color.h"
 
 template <typename T,
         typename std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
@@ -40,3 +43,61 @@ inline ::testing::AssertionResult equal_within_error(
                 << +(lhs - rhs) << " (tolerance=" << +error_bound << ").";
     }
 }
+
+template <typename T, typename Color>
+inline ::testing::AssertionResult assert_colors_near(const char* lhs_expr,
+        const char* rhs_expr,
+        const char* tol_expr,
+        const Color& lhs_color,
+        const Color& rhs_color,
+        T error_tol) {
+    bool is_failure = false;
+    T max_error = 0.0;
+
+    color::for_each_element(lhs_color,
+            [&is_failure, &rhs_color, &max_error, error_tol](
+                                    auto elem, std::size_t idx) {
+                auto diff = std::abs(elem - T(rhs_color.as_array()[idx]));
+                if(diff > error_tol) {
+                    is_failure = true;
+                    max_error = std::max(diff, max_error);
+                }
+            });
+
+    if(is_failure) {
+        return ::testing::AssertionFailure()
+                << "Colors `" << lhs_expr << "` and `" << rhs_expr
+                << "` differ by more than " << error_tol
+                << " in one or more channels.\n\t" << lhs_color << " ~ "
+                << rhs_color << "\n\tMaximum channel difference: " << max_error;
+    } else {
+        return ::testing::AssertionSuccess();
+    }
+}
+template <typename Color>
+inline ::testing::AssertionResult assert_colors_equal(const char* lhs_expr,
+        const char* rhs_expr,
+        const Color& lhs_color,
+        const Color& rhs_color) {
+    bool is_failure = false;
+
+    color::for_each_element(lhs_color,
+            [&is_failure, &rhs_color](auto elem, std::size_t idx) {
+                if(elem != rhs_color.as_array()[idx]) {
+                    is_failure = true;
+                }
+            });
+
+    if(is_failure) {
+        return ::testing::AssertionFailure()
+                << "Colors `" << lhs_expr << "` and `" << rhs_expr
+                << "` are not equal:\n\t" << lhs_color << " != " << rhs_color;
+    } else {
+        return ::testing::AssertionSuccess();
+    }
+}
+
+#define ASSERT_COLORS_EQ(c1, c2)                                               \
+    ASSERT_PRED_FORMAT2(assert_colors_equal, c1, c2)
+#define ASSERT_COLORS_NEAR(c1, c2, tol)                                        \
+    ASSERT_PRED_FORMAT3(assert_colors_near, c1, c2, tol)
